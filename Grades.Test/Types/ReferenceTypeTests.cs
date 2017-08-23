923c1e5929161b90e2e983530d4c04e16fe04711
Joel Ramos <joelalexanderramos@gmail.com>
2017-08-23 17:14:41 -0400

    Today changes!

--- Grades.Test/Types/ReferenceTypeTests.cs
+++ Grades.Test/Types/ReferenceTypeTests.cs
@@ -1,15 +1,85 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Grades.Test.Types
 {
     [TestClass]
-    class ReferenceTypeTests
+    public class TypeTests
     {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "joel";
+            name = name.ToUpper();
+
+            Assert.AreEqual("JOEL", name);
+        }
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(ref x);
+
+            Assert.AreEqual(47, x);           
+
+        }
+
+        private void IncrementNumber(ref int number)
+        {
+             number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            book2.Name = "A GradeBook 1";
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook 2", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook b2)
+        {
+            b2.Name = "A GradeBook 2";
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "Scott";
+            string name2 = "Scott";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCulture);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void IntVariablesHoldAValue()
         {
@@ -26,7 +96,7 @@ namespace Grades.Test.Types
             GradeBook b1 = new GradeBook();
             GradeBook b2 = b1;
             
-            b1 = new GradeBook();
+            //b1 = new GradeBook();
             b1.Name = "Joel Ramos";
 
             Assert.AreEqual(b1.Name, b2.Name);
