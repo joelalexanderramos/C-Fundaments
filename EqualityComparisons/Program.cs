97440549bb5c7b76bbccae4efb766be90659a42d
Joel Ramos <joelalexanderramos@gmail.com>
2017-08-28 21:05:48 -0400

    Pending changes!

---
+++ EqualityComparisons/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityComparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Console.WriteLine("Operator: " + AreIntsEqualOp(3,3));
+            //Console.WriteLine("Method: " + AreIntsEqualMethod(3, 3));           
+
+            //string s1 = "Joel";
+            //string s2 = "Joel";
+
+            //Console.WriteLine(s1 == s2);
+
+            // -- STRING COMPARISON
+            //string s1 = "Joel";
+            //string s2 = "joel";
+
+            //s1.Equals(s2, StringComparison.OrdinalIgnoreCase);
+        }
+
+        static bool AreIntsEqualOp(int x, int y)
+        {
+            return x == y;
+        }
+
+        static bool AreIntsEqualMethod(int x, int y)
+        {
+            return x.Equals(y);
+        }
+    }
+}
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -7,14 +7,17 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+
             IGradeTracker book = CreateGradeBook();
-            
+
             //GetBookName(book);
             AddGrades(book);
             SaveGrades(book);
             WriteResults(book);
         }
         
+       
+
 
         private static IGradeTracker CreateGradeBook()
         {
